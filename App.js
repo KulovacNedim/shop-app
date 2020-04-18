1b7bee4fd6663566092508e13021bd97db492fbb
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-18 12:56:50 +0200

    redux setup. dammy-data used while settings app up

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './store/reducers/products';
+
+const rootReducers = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(rootReducers);
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View></View>
+    </Provider>
   );
 }
 
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import { FlatList, View, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+    return (
+        <View ></View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductsOverviewScreen;
