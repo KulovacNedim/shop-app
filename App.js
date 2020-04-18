a18ac7f9172e165c5b898435e228e297dfaf539e
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-18 11:31:03 +0200

    first commit. app created with the expo

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
