46807bfcaaf68b145f12dcd2d54b5b0a95eb2dcb
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-05-07 02:46:57 +0200

    EditProductScreen populated with the dummy text. Navigation t this screen implemented from the edit product button with the productId property and from the create product icon without productId to be able to diferentiate between two modes

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Image, Text, Button, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
-
-import Colors from '../../constants/Colors';
+import { View, StyleSheet, Image, Text, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 
 const ProductItem = props => {
     let TouchableCmp = TouchableOpacity;
@@ -12,7 +10,7 @@ const ProductItem = props => {
     return (
         <View style={styles.product}>
             <View style={styles.touchable}>
-                <TouchableCmp onPress={props.onViewDetail} useForeground>
+                <TouchableCmp onPress={props.onSelect} useForeground>
                     <View>
                         <View style={styles.imageContainer}>
                             <Image style={styles.image} source={{ uri: props.image }} />
@@ -22,8 +20,7 @@ const ProductItem = props => {
                             <Text style={styles.price}>${props.price.toFixed(2)}</Text>
                         </View>
                         <View style={styles.actions}>
-                            <Button color={Colors.primary} title="View Details" onPress={props.onViewDetail} />
-                            <Button color={Colors.primary} title="To Cart" onPress={props.onAddToCart} />
+                            {props.children}
                         </View>
                     </View>
                 </TouchableCmp>
@@ -61,7 +58,7 @@ const styles = StyleSheet.create({
     },
     details: {
         alignItems: 'center',
-        height: '15%',
+        height: '17%',
         padding: 10
     },
     title: {
@@ -78,7 +75,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        height: '25%',
+        height: '23%',
         paddingHorizontal: 20
     }
 });
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,10 +1,20 @@
 import PRODUCTS from '../../data/dummy-data';
+import * as productActions from '../actions/products';
 
 const initialState = {
     availableProducts: PRODUCTS,
-    userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1 ')
+    userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
 };
 
 export default (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case productActions.DELETE_PRODUCT:
+            return {
+                ...state,
+                userProducts: state.userProducts.filter(product => product.id !== action.pid),
+                availableProducts: state.availableProducts.filter(product => product.id !== action.pid)
+            };
+        default:
+            return state;
+    }
 };
