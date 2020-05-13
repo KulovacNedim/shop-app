8d8a986934dfecbe390fd33cdbbca591a3c926f4
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-05-13 23:52:34 +0200

    admin CRUD operations over products implemented. Card component created to be reusible and placed in all places where card look is used

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -14,6 +14,40 @@ export default (state = initialState, action) => {
                 userProducts: state.userProducts.filter(product => product.id !== action.pid),
                 availableProducts: state.availableProducts.filter(product => product.id !== action.pid)
             };
+        case productActions.UPDATE_PRODUCT:
+            const productIndex = state.userProducts.findIndex(
+                prod => prod.id === action.pid
+            );
+            const updatedProduct = new Product(
+                action.pid,
+                state.userProducts[productIndex].ownerId,
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                state.userProducts[productIndex].price
+            );
+            const updatedUserProducts = [...state.userProducts];
+            updatedUserProducts[productIndex] = updatedProduct;
+            const availableProductIndex = state.availableProducts.findIndex(
+                prod => prod.id === action.pid
+            );
+            const updatedAvailableProducts = [...state.availableProducts];
+            updatedAvailableProducts[availableProductIndex] = updatedProduct;
+            return {
+                ...state,
+                availableProducts: updatedAvailableProducts,
+                userProducts: updatedUserProducts
+            };
+        case productActions.DELETE_PRODUCT:
+            return {
+                ...state,
+                userProducts: state.userProducts.filter(
+                    product => product.id !== action.pid
+                ),
+                availableProducts: state.availableProducts.filter(
+                    product => product.id !== action.pid
+                )
+            };
         default:
             return state;
     }
