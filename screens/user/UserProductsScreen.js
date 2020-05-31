024e5f3d5c6d547fc94a2f30d6397dfc6a8ef2b2
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-05-31 16:52:40 +0200

    orders mapped to users. user id used to store products and orders to database and to query data from database

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FlatList, Button, StyleSheet } from 'react-native';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { View, Text, FlatList, Button, Platform, Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
@@ -16,37 +16,54 @@ const UserProductsScreen = props => {
         props.navigation.navigate('EditProduct', { productId: id });
     };
 
+    const deleteHandler = id => {
+        Alert.alert('Are you sure?', 'Do you really want to delete this item?', [
+            { text: 'No', style: 'default' },
+            {
+                text: 'Yes',
+                style: 'destructive',
+                onPress: () => {
+                    dispatch(productsActions.deleteProduct(id));
+                }
+            }
+        ]);
+    };
+
+    if (userProducts.length === 0) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>No products found, maybe start creating some?</Text>
+            </View>
+        );
+    }
+
     return (
         <FlatList
             data={userProducts}
-            keyExtractor={item => item.id.toString()}
-            renderItem={itemData => {
-                return (
-                    <ProductItem
-                        title={itemData.item.title}
-                        image={itemData.item.imageUrl}
-                        price={itemData.item.price}
-                        onSelect={() => {
-                            editProductHandler(itemData.item.id)
+            keyExtractor={item => item.id}
+            renderItem={itemData => (
+                <ProductItem
+                    image={itemData.item.imageUrl}
+                    title={itemData.item.title}
+                    price={itemData.item.price}
+                    onSelect={() => {
+                        editProductHandler(itemData.item.id);
+                    }}
+                >
+                    <Button
+                        color={Colors.primary}
+                        title="Edit"
+                        onPress={() => {
+                            editProductHandler(itemData.item.id);
                         }}
-                    >
-                        <Button
-                            color={Colors.primary}
-                            title="Edit"
-                            onPress={() => {
-                                editProductHandler(itemData.item.id)
-                            }}
-                        />
-                        <Button
-                            color={Colors.primary}
-                            title="Delete"
-                            onPress={() => {
-                                dispatch(productsActions.deleteProduct(itemData.item.id));
-                            }}
-                        />
-                    </ProductItem>
-                )
-            }}
+                    />
+                    <Button
+                        color={Colors.primary}
+                        title="Delete"
+                        onPress={deleteHandler.bind(this, itemData.item.id)}
+                    />
+                </ProductItem>
+            )}
         />
     );
 };
@@ -61,7 +78,8 @@ UserProductsScreen.navigationOptions = navData => {
                     iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
                     onPress={() => {
                         navData.navigation.toggleDrawer();
-                    }} />
+                    }}
+                />
             </HeaderButtons>
         ),
         headerRight: () => (
@@ -71,12 +89,11 @@ UserProductsScreen.navigationOptions = navData => {
                     iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
                     onPress={() => {
                         navData.navigation.navigate('EditProduct');
-                    }} />
+                    }}
+                />
             </HeaderButtons>
         )
-    }
+    };
 };
-
-const styles = StyleSheet.create({});
 
 export default UserProductsScreen;
--- store/actions/products.js
+++ store/actions/products.js
@@ -1,12 +1,14 @@
+import Product from '../../models/product';
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 
-import Product from '../../models/product';
-
 export const fetchProducts = () => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    // any async code you want!
+    const userId = getState().auth.userId;
     try {
       const response = await fetch(
         'https://shopp-app-c4518.firebaseio.com/products.json'
@@ -23,7 +25,7 @@ export const fetchProducts = () => {
         loadedProducts.push(
           new Product(
             key,
-            'u1',
+            resData[key].ownerId,
             resData[key].title,
             resData[key].imageUrl,
             resData[key].description,
@@ -32,7 +34,11 @@ export const fetchProducts = () => {
         );
       }
 
-      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      dispatch({
+        type: SET_PRODUCTS,
+        products: loadedProducts,
+        userProducts: loadedProducts.filter(prod => prod.ownerId === userId)
+      });
     } catch (err) {
       // send to custom analytics server
       throw err;
@@ -59,9 +65,11 @@ export const deleteProduct = productId => {
 
 export const createProduct = (title, description, imageUrl, price) => {
   return async (dispatch, getState) => {
+    // any async code you want!
     const token = getState().auth.token;
+    const userId = getState().auth.userId;
     const response = await fetch(
-      'https://shopp-app-c4518.firebaseio.com/products.json?auth=${token}',
+      `https://shopp-app-c4518.firebaseio.com/products.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
@@ -71,7 +79,8 @@ export const createProduct = (title, description, imageUrl, price) => {
           title,
           description,
           imageUrl,
-          price
+          price,
+          ownerId: userId
         })
       }
     );
@@ -85,7 +94,8 @@ export const createProduct = (title, description, imageUrl, price) => {
         title,
         description,
         imageUrl,
-        price
+        price,
+        ownerId: userId
       }
     });
   };
